23d1ac2c588da8d50ae5e8cb43f705905b380ed9
AwesomeMc101 <43798963+AwesomeMc101@users.noreply.github.com>
2024-03-17 03:33:07 -0500

    Add files via upload

---
+++ include/auroria_types.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <vector>
+
+#define AR_USE_MATH 
+#define AR_USE_BASIC 
+
+
+
+#define AR_TYPE_FLOAT 1
+#define AR_TYPE_INT 2
+#define AR_TYPE_STRING 3
+#define AR_TYPE_CHAR 4
+#define AR_TYPE_BOOL 5
+#define AR_TYPE_NIL 6
+#define AR_TYPE_CHARSET 9
+
+#define AR_TYPE_TOKEN 7 // ; ' ( )
+#define AR_TYPE_KEY 8 // true, false, if
+
+#define AR_TYPE_SYSTEM 10
+
+#define AR_TYPE short
+
